e5e686c4c5f7868ceefa962b48023a3f002e801c
Ruan Carlos <ruancar500@gmail.com>
2026-05-26 16:57:28 -0300

    Enhance user retrieval with error handling
    Added error handling for user retrieval, including checks for non-existent and inactive users.

--- app/dependencies.py
+++ app/dependencies.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from app.database import get_db
-from app.exceptions import AcessoNegadoError
+from app.exceptions import AcessoNegadoError, UsuarioNaoEncontradoError, UsuarioDesativadoError
 from app.models import Usuario
 from app.security import verificar_token
 
@@ -12,6 +12,13 @@ async def obter_usuario_logado(usuario_id: int = Depends(verificar_token), db: A
     query = select(Usuario).where(Usuario.id == usuario_id)
     resultado = await db.execute(query)
     usuario = resultado.scalars().first()
+
+    if not usuario:
+        raise UsuarioNaoEncontradoError()
+
+    if not usuario.ativo:
+        raise UsuarioDesativadoError()
+    
     return usuario
 
 async def obter_admin_logado(usuario_id: int = Depends(verificar_token), db: AsyncSession = Depends(get_db)) -> Usuario:
@@ -20,6 +27,6 @@ async def obter_admin_logado(usuario_id: int = Depends(verificar_token), db: Asy
     usuario = resultado.scalars().first()
 
     if not usuario.admin:
-        raise AcessoNegadoError("Você prrecisa ser admin")
+        raise AcessoNegadoError("Disponível somente para administradores")
     
     return usuario
